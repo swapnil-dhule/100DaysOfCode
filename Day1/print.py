8d62cd441922a1712c5cf963a0854010c7b2285f
swapnil-dhule <swapneel.dhule@gmail.com>
2023-01-31 13:40:53 +0530

    Day1

---
+++ Day1/print.py
@@ -0,0 +1,1 @@
+print(f"Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
---
+++ Day1/string_length.py
@@ -0,0 +1,14 @@
+# # Approach-1
+
+# str1 = input("Enter the string: ")
+# print(f"Length of {str1} is {len(str1)}")
+
+# Approach-2
+
+str2 = input("Enter the string: ")
+count = 0
+
+for i in str2:
+    count = count + 1
+
+print(f"Length of {str2} is {count}")
