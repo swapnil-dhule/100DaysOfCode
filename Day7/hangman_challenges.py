3073baf0d05d9ff41585235a0311e48a68256e61
swapnil-dhule <swapneel.dhule@gmail.com>
2023-02-18 16:43:59 +0530

    Day 7

--- Day7/hangman_challenges.py
+++ Day7/hangman_challenges.py
@@ -1,30 +1,93 @@
 import random
 
-word_list = ["ardvark", "baboon", "camel"]
+word_list = [ "POOL", "MAMA", "EGG", "FIRE", "ARM", "SUN", "DINNER", "FREE", "HORSE", "BOOK", "ICE", "SEA", "HOME", "CROSS", "FUNNY" ]
+
+hangmanpics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+lives = 7
 
 chosen_word = random.choice(word_list)
 
 print(f"Test - {chosen_word}")
 
-guess = input("Guess a letter: ").lower()
-
 display = []
 
-
 for i in chosen_word:
-    # if guess == i:
-    #     print("Right")
-    # else:
-    #     print("Wrong")
     display.append("_")
 
-# print(display)
+print(display)
 
-while "_" not in display:
+counter = 0
+
+while "_" in display:
     index_display = 0
+    guess = input("Guess a letter: ").lower()
     for i in chosen_word:
         if guess == i:
             display[index_display] = i
         index_display += 1
+    
+    if guess not in chosen_word:
+        lives -= 1
+        counter += 1
+        print(f"Counter is {counter}")
 
-print(display)
+    if counter > 0:
+        print(hangmanpics[counter-1])
+
+    if lives == 0:
+        print("You lose!")
+        exit()
+    
+    print(display)
+
+print(f"You win!")
