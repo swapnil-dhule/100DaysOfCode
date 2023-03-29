78f280584753ee9ca168917c9834bcffd0273e0e
swapnil-dhule <swapneel.dhule@gmail.com>
2023-03-29 11:53:53 +0530

    Day10

--- Day10/calculator.py
+++ Day10/calculator.py
@@ -12,18 +12,49 @@ def operations():
         result = a/b
         return result
 
+import os
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+print(logo)
+
 a = int(input("What's the first number?: "))
 print(f"+\n-\n*\n/")
 operation = input("Pick an operation: ")
 b = int(input("What's the next number?: "))
-loop = "y"
 
-while loop == "y":
+
+while True:
     output = operations()
     print(f"{a} {operation} {b} = {output}")
     loop = input(f"Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation: ")
+    
     if loop == "y":
         a = output
         operation = input("Pick an operation: ")
         b = int(input("What's the next number?: "))
-        
+    elif loop == "n":
+        os.system('cls')
+        print(logo)
+        a = int(input("What's the first number?: "))
+        print(f"+\n-\n*\n/")
+        operation = input("Pick an operation: ")
+        b = int(input("What's the next number?: "))
+
+
+
---
+++ Day10/format_name.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+
+    F_name = f_name.title()
+    L_name = l_name.title()
+
+    return f"{F_name} {L_name}"
+
+print(format_name("swapnil", "dhule"))
